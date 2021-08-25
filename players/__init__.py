586e94b44ce54e2e7c303d4486080544a348bcfa
Francois Parenti <francois.parenti@gmail.com>
2021-08-25 15:59:15 +0200

    fixing init

--- players/__init__.py
+++ players/__init__.py
@@ -1,3 +1,3 @@
-# from .create_player import PlayerCreationController
-# from .menu import PlayerMenuController
-# from .change_rank import PlayerRankController
+from .create_player import PlayerCreationController
+from .menu import PlayerMenuController
+from .change_rank import PlayerRankController
--- tournaments/__init__.py
+++ tournaments/__init__.py
@@ -1,2 +1,2 @@
+from .create_tournament import TournamentCreationController, CreateTournament, LoadTournament
 # from controllers import LoadTournament, CreateTournament, TournamentCreationController, PlayerCreationController
-# from .create_tournament import TournamentCreationController, CreateTournament, LoadTournament
